37008af351689c3a6cfd3821a72033c573650644
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-10-25 20:37:44 -0300

    Feat: SubCommands parse

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,25 @@ mod args;
 fn main() {
     let args = TodoArgs::parse();
 
-    println!("{:?}", args);
+    match args.action_type {
+        ActionType::New(new_args) => {
+            // TODO: Implement the function
+        }
+
+        ActionType::Add(add_args) => {
+            // TODO: Implement the function
+        }
+
+        ActionType::Done(done_args) => {
+            // TODO: Implement the function
+        }
+
+        ActionType::UnDone(undone_args) => {
+            // TODO: Implement the function
+        }
+
+        ActionType::Remove(remove_args) => {
+            // TODO: Implement the function
+        }
+    }
 }

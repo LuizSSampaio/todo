dcddbb51e15cd45a8f208d062b500ac9cdd5918e
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-10-28 08:10:58 -0300

    Feat: Added db connect

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,8 @@
-use rusqlite::{Connection, Result};
+use rusqlite::{Connection, Result, NO_PARAMS};
+use std::path::Path;
+
+const DB_PATH: String = "todo.db";
+const DB_TABLE_NAME: String = "todo";
 
 #[derive(Debug)]
 struct To_Do {
@@ -6,7 +10,30 @@ struct To_Do {
     is_done: bool,
 }
 
-fn connect() {}
+fn connect() -> rusqlite::Connection {
+    let connection = Connection::open(DB_PATH)?;
+
+    let table_exists: bool = connection.query_row(
+        &format!(
+            "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='{}'",
+            DB_TABLE_NAME
+        ),
+        NO_PARAMS,
+        |row| row.get(0),
+    )?;
+
+    if !table_exists {
+        connection.execute(
+            &format!(
+                "CREATE TABLE {} (id INTEGER PRIMARY KEY, name TEXT, is_done BOOLEAN)",
+                DB_TABLE_NAME
+            ),
+            NO_PARAMS,
+        )?;
+    }
+
+    connection
+}
 
 fn create_new_list() {}
 

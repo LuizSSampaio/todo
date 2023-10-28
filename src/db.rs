2b768162106e384d150831cc2cf17338f033f236
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-10-28 07:46:55 -0300

    Feat: Added db functions sckeleton

---
+++ src/db.rs
@@ -0,0 +1,19 @@
+use rusqlite::{Connection, Result};
+
+#[derive(Debug)]
+struct To_Do {
+    name: String,
+    is_done: bool,
+}
+
+fn connect() {}
+
+fn create_new_list() {}
+
+fn add_list_item() {}
+
+fn remove_list_item() {}
+
+fn done_list_item() {}
+
+fn undone_list_item() {}

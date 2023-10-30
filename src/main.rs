f731864ede36643aa7201330c0e084b5f126dee3
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-10-30 16:43:12 -0300

    Removed sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,1 @@
-use rusqlite::{Connection, Result};
-
-fn main() {
-    let conn = Connection::open("todo.db").unwrap();
-    conn.execute(
-        "create table if not exists todo (
-            id integer primary key, 
-            title text not null, 
-            is_done boolean not null
-        )",
-        [],
-    )
-    .unwrap();
-}
+fn main() {}
